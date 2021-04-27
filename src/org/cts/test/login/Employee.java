632a1ea66336fba495e6d7dfe0ca50d362794547
suvanesh007 <suvanesansundar@gmail.com>
2021-04-27 15:14:38 +0530

    my task

---
+++ src/org/cts/test/login/Employee.java
@@ -0,0 +1,21 @@
+package org.cts.test.login;
+
+public class Employee {
+public static void main(String[] args) {
+	//datatype varName=Value;
+	byte age=12;
+	System.out.println("Age is "+ age);
+	short pin=6666;
+	System.out.println("Pin is "+pin);
+	int Id=2345678;
+	System.out.println("Id is "+Id);
+	char gender='M';
+	System.out.println("Gender is "+gender);
+	String name="Suvanesh";
+	System.out.println("name is "+name);
+	float sal=30000;
+	System.out.println("salary is "+sal);
+	String dob="21/3/2021";
+	System.out.println("dob is "+dob);
+}
+}
